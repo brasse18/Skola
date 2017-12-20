c7d00d8d432b89b26998448907d0c7559a2d2f91
brasse <brasse.bb@gmail.com>
2017-12-20 11:57:13 +0100

    edit linux uppgift4: fixt bugs in script1.bash

--- Programering/linux/uppgift4/script1.bash
+++ Programering/linux/uppgift4/script1.bash
@@ -2,7 +2,15 @@
 
 DOM=$1
 SITE=$2
+SITEDIR=$(echo "$SITE" | sed 's:[a-z][^/]*[.]html::g')
 
-ANSER=$(curl -s $DOM$SITE | sed '/<head/,/head>/d' | grep -o 'src=.*"\|href=".*"' | grep -o '".*"' | tr -d '"')
-echo $ANSER | tr ' ' '\n' | sed -e 's/^/\//;' -e 's/\/\//\//'
-#echo $ANSER | sed -e "s/mechani.se//"
+ANSER=$(curl -s $DOM$SITE | sed '/<head/,/head>/d' | grep -o 'src=.*"\|href="[^"]*"' | grep -o '".*"' | tr -d '"')
+ANSER=$(echo "$ANSER" | sed -e '/@/d' -e '/http:/d' -e 's:^[^/]:'$SITEDIR'&:')
+
+ANSER=$(echo "$ANSER" | sed 's/\([a-z][^/]*[/][.][.][/]\)//')
+
+ANSER=$(echo "$ANSER" | tr ' ' '\n')
+ANSER=$(echo "$ANSER" | sed -e 's!^!'$DOM' '$SITE' '$DOM' !')
+
+echo "$ANSER" | sort -u
+
--- Programering/linux/uppgift4/script2.bash
+++ Programering/linux/uppgift4/script2.bash
@@ -2,11 +2,15 @@
 
 DOME="mechani.se"
 
-./script1.bash $DOME /unix/public/cw4a.html > output.temp
-TEXT=$(cat output.temp)
+./script1.bash $DOME /unix/public/cw4a.html > out.temp
+TEXT=$(cat out.temp)
+echo "$TEXT" > done.txt
+TEMPTEXT=""
 
 for fn in $TEXT; do
-	./script1.bash $DOME $fn >> pros.txt
+	temp=$(echo "$fn" | awk '{print $4}')
+	temp=$() 
+	$TEMPTEXT=$(bash script1.bash $DOME $fn)
 	echo $fn
 done
 
